7aa5410bf2d1962167254a7802d9b45c6cda1d21
Даниил Сурин <dan1surin@gmail.com>
2023-01-19 19:01:14 +0300

    hw03

---
+++ HW003/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+int[] FillArray(int size)
+{
+    int[] arr = new int[size]; //массив знает свою длинну если выделить под него память такой операцией
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите {i} элемент");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i] + " ");
+    }
+}
+
+int[] array = FillArray(8);
+Console.WriteLine(string.Join(",",array));
